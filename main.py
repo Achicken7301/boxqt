8c627e60d3c84d5a99df87abea73778efa23b96c
Khang <banhbeovodung01@gmail.com>
2022-06-30 10:55:07 +0700

    small update

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-from operator import is_
 from PyQt5 import QtWidgets, uic
 import sys
-from PyQt5 import QtWidgets, QtCore
-from pandas import wide_to_long
-from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
-import sys  # We need sys so that we can pass argv to QApplication
-import os
-from random import randint
 import serial
 import datetime
 import csv
+from PyQt5 import QtWidgets, QtCore
 
-ser = serial.Serial(port='COM6', baudrate=115200)
+ser = serial.Serial(port='COM6', baudrate=921600)
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
+        # self.is_record = False
         self.is_record = False
+        self.force = []
         self.acceleration_x = []
         self.acceleration_y = []
         self.acceleration_z = []
@@ -31,61 +27,15 @@ class MainWindow(QtWidgets.QMainWindow):
         # Load the UI Page
         uic.loadUi('src/ui/main.ui', self)
 
-        length = 500
-
-        self.x = list(range(length))
-        self.accel_x = [0 for _ in range(length)]
-        self.accel_y = [0 for _ in range(length)]
-        self.accel_z = [0 for _ in range(length)]
-        self.gyro_x = [0 for _ in range(length)]
-        self.gyro_y = [0 for _ in range(length)]
-        self.gyro_z = [0 for _ in range(length)]
-
-        self.acceleration.setBackground('w')
-        self.gyroscope.setBackground('w')
-
-        # Add legend (line name)
-        self.acceleration.addLegend()
-        self.gyroscope.addLegend()
-
-        # Add Title
-        self.acceleration.setTitle("Acceleration", color="b", size="20pt")
-        self.gyroscope.setTitle("Gyroscope", color="b", size="20pt")
-
-        width = 1
-        pen_accel_x = pg.mkPen('r', width=width)
-        pen_accel_y = pg.mkPen('b', width=width)
-        pen_accel_z = pg.mkPen('g', width=width)
-        pen_gyro_x = pg.mkPen('c', width=width)
-        pen_gyro_y = pg.mkPen('m', width=width)
-        pen_gyro_z = pg.mkPen('k', width=width)
-
-        self.data_line_accel_x = self.acceleration.plot(
-            self.x, self.accel_x, pen=pen_accel_x, name="accel_x")
-        self.data_line_accel_y = self.acceleration.plot(
-            self.x, self.accel_y, pen=pen_accel_y, name="accel_y")
-        self.data_line_accel_z = self.acceleration.plot(
-            self.x, self.accel_z, pen=pen_accel_z, name="accel_z")
-        self.data_line_gyro_x = self.gyroscope.plot(
-            self.x, self.gyro_x, pen=pen_gyro_x, name="gyro_x")
-        self.data_line_gyro_y = self.gyroscope.plot(
-            self.x, self.gyro_y, pen=pen_gyro_y, name="gyro_y")
-        self.data_line_gyro_z = self.gyroscope.plot(
-            self.x, self.gyro_z, pen=pen_gyro_z, name="gyro_z")
-
-        # button
+        # # button
         self.startRecordBtn.clicked.connect(lambda: self.start_record())
         self.stopRecordBtn.clicked.connect(lambda: self.stop_record())
 
         # ... init continued ...
         self.timer = QtCore.QTimer()
-
-        # Update value to plot
-        # self.timer.timeout.connect(lambda: self.update_data_imu_on_bag())
-
         # Recording data
         self.timer.timeout.connect(lambda: self.record_data())
-
+        # self.timer.timeout.connect(lambda: self.update_data_force_on_bag())
         self.timer.start()
 
     def start_record(self):
@@ -98,7 +48,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def stop_record(self):
         self.export_to_csv()
-        self.sample_rate.setText("Stop Recording")
+        self.sample_rate.setText("Stop")
         print("Stop Recording")
         # Empty data before record a new one
         self.is_record = False
@@ -115,25 +65,24 @@ class MainWindow(QtWidgets.QMainWindow):
             b = ser.readline()
             try:
                 string_n = b.decode()
-                [a_x, a_y, a_z, g_x, g_y, g_z] = string_n.split()
-                print(a_x + " " + a_y + " " + a_z +
-                      " " + g_x + " " + g_y + " " + g_z)
-
+                [force, a_x, a_y, a_z, g_x, g_y, g_z] = string_n.split()
+                print(force + ' ' + a_x + ' ' + a_y + ' ' + a_z + ' ' + g_x + ' ' + g_y + ' ' + g_z + ' ')
+                self.force.append(float(force))
                 self.acceleration_x.append(float(a_x))
                 self.acceleration_y.append(float(a_y))
                 self.acceleration_z.append(float(a_z))
                 self.gyroscope_x.append(float(g_x))
                 self.gyroscope_y.append(float(g_y))
                 self.gyroscope_z.append(float(g_z))
-            except Exception:
-                pass
-        else:
-            pass
+            except Exception as e:
+                print(b)
+                print(e)
 
     def export_to_csv(self):
         # import rows to columns
         # https://stackoverflow.com/questions/4155106/python-csv-write-by-column-rather-than-row
         l = [
+            self.force,
             self.acceleration_x,
             self.acceleration_y,
             self.acceleration_z,
@@ -143,7 +92,7 @@ class MainWindow(QtWidgets.QMainWindow):
         ]
         data = zip(*l)
 
-        header = ['a_x', 'a_y', 'a_z', 'g_x', 'g_y', 'g_z']
+        header = ['force', 'a_x', 'a_y', 'a_z', 'g_x', 'g_y', 'g_z']
 
         # filename: dd-mm-YYYY hh:mm:ss
         name_format = datetime.datetime.now().strftime("%x %X").replace("/", "-")
@@ -152,48 +101,47 @@ class MainWindow(QtWidgets.QMainWindow):
 
         with open(filename, 'w', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
-
             writer.writerow(header)
-
             # write multiple rows
             writer.writerows(data)
 
-    def update_data_imu_on_bag(self):
-        self.acceleration.x = self.x[1:]  # Remove the first y element.
+    def update_data_force_on_bag(self):
+
+        length = 500
+
+        self.x = list(range(length))
+        self.force = [0 for _ in range(length)]
+        self.force_graph.setBackground('w')
+
+        # # Add legend (line name)
+        self.force_graph.addLegend()
+
+        # # Add Title
+        self.force_graph.setTitle("Force Graph", color="b", size="10pt")
+
+        width = 1
+        pen_force_graph = pg.mkPen('r', width=width)
+
+        self.data_line_force_graph = self.force_graph.plot(
+            self.x, self.force, pen=pen_force_graph, name="force")
+
+        self.x = self.x[1:]  # Remove the first y element.
         # Add a new value 1 higher than the last.
-        self.acceleration.x.append(self.x[-1] + 1)
+        self.x.append(self.x[-1] + 1)
         b = ser.readline()
         # Cannot decode the 1st value b'\xb4j7\r\n'
         # solve this with try except
         try:
             string_n = b.decode()
-            [a_x, a_y, a_z, g_x, g_y, g_z] = string_n.split()
-            # print(a_x + " " + a_y + " " + a_z +
-            #       " " + g_x + " " + g_y + " " + g_z)
+            print(string_n)
+            [force, a_x, a_y, a_z, g_x, g_y, g_z] = string_n.split()
             # Acceleration
-            self.accel_x = self.accel_x[1:]
-            self.accel_y = self.accel_y[1:]
-            self.accel_z = self.accel_z[1:]
-            self.accel_x.append(float(a_x))
-            self.accel_y.append(float(a_y))
-            self.accel_z.append(float(a_z))
-            # Gyroscope
-            self.gyro_x = self.gyro_x[1:]
-            self.gyro_y = self.gyro_y[1:]
-            self.gyro_z = self.gyro_z[1:]
-            self.gyro_x.append(float(g_x))
-            self.gyro_y.append(float(g_y))
-            self.gyro_z.append(float(g_z))
-        except Exception:
-            pass
-
+            self.force = self.force[1:]
+            self.force.append(float(force))
+        except Exception as e:
+            print(e)
         # Update the data.
-        self.data_line_accel_x.setData(self.x, self.accel_x)
-        self.data_line_accel_y.setData(self.x, self.accel_y)
-        self.data_line_accel_z.setData(self.x, self.accel_z)
-        self.data_line_gyro_x.setData(self.x, self.gyro_x)
-        self.data_line_gyro_y.setData(self.x, self.gyro_y)
-        self.data_line_gyro_z.setData(self.x, self.gyro_z)
+        self.data_line_force_graph.setData(self.x, self.force)
 
 
 def main():
