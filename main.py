7728ba4b0dd02aa0b3d211fcda28b79fe36f2c24
Khang <55547021+Achicken7301@users.noreply.github.com>
2022-08-18 12:54:46 +0700

    Feat(main.py)
    Record 2 COM at once.
    Export .csv to each COM

--- main.py
+++ main.py
@@ -1,95 +1,120 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 import sys
 import glob
 import serial
 import datetime
 import csv
 import time
-from PyQt5 import QtWidgets, QtCore
-import concurrent.futures
 
 
 class MainWindow(QtWidgets.QMainWindow):
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.port = None
+        self.port_on_hand = None
+        self.port_on_bag = None
         self.is_record = False
         # Load the UI Page
-        uic.loadUi("src/ui/main.ui", self)
-        # self.port_scan()
-        self.startRecordBtn.clicked.connect(self.start_record)
-        self.stopRecordBtn.clicked.connect(self.stop_record)
-        self.portScan.clicked.connect(self.port_scan)
+        uic.loadUi("src/ui/DevMainWindow.ui", self)
+        # self.portScan()
+        self.startRecordBtn.clicked.connect(self.startRecord)
+        self.stopRecordBtn.clicked.connect(self.stopRecord)
+        self.uiPortScan.clicked.connect(self.portScan)
         self.clearListBtn.clicked.connect(self.clearList)
 
     def clearList(self):
         print("clear list")
-        self.listPorts.clear()
+        self.portOnHand.clear()
+        self.portOnBag.clear()
 
-    def start_record(self):
+    def startRecord(self):
         if self.is_record == True:
             print("Already Recording")
         else:
+            print("Set is_record to True")
             self.is_record = True
-            self.ser = serial.Serial(port=self.port)
+            print("Connect port ser on hand")
+            self.ser_on_hand = serial.Serial(port=self.port_on_hand)
+            print("Connect port ser on bag")
+            self.ser_on_bag = serial.Serial(port=self.port_on_bag)
             self.sample_rate.setText("Recording")
-            self.data = []
+            self.data_on_bag = []
+            self.data_on_hand = []
             print("Start Recording")
 
             # ... init continued ...
             self.timer = QtCore.QTimer()
-            self.timer.timeout.connect(lambda: self.record_data())
+            self.timer.timeout.connect(lambda: self.recordData())
             self.timer.start()
 
-    def stop_record(self):
+    def stopRecord(self):
         if self.is_record == True:
             self.is_record = False
-            self.ser.close()
+            self.ser_on_hand.close()
+            self.ser_on_bag.close()
             self.sample_rate.setText("Stop")
             print("Stop Recording")
             # Empty data before record a new one
             self.export_to_csv()
-            self.data = []
+            self.data_on_bag = []
+            self.data_on_hand = []
         else:
             print("Haven't record anything yet")
 
-    def record_data(self):
+    def recordData(self):
         if self.is_record == True:
             try:
-                b = self.ser.readline()
-                string_n = b.decode().splitlines()
-                self.data.append(string_n)
+                data_ser_on_bag = self.ser_on_bag.readline()
+                data_ser_on_bag = data_ser_on_bag.decode().splitlines()
+
+                data_ser_on_hand = self.ser_on_hand.readline()
+                data_ser_on_hand = data_ser_on_hand.decode().splitlines()
+
+                self.data_on_bag.append(data_ser_on_hand)
+                self.data_on_hand.append(data_ser_on_bag)
             except Exception as e:
                 print(e)
 
     def export_to_csv(self):
-        header = ["force a_x a_y a_z g_x g_y g_z"]
         # filename: dd-mm-YYYY hh:mm:ss
-        name_format = datetime.datetime.now().strftime("%x %X").replace("/", "-")
+        name_format = datetime.datetime.now().strftime("%x %X").replace(
+            "/", "-")
         name_format = name_format.replace(":", "-")
         filename = name_format + ".csv"
 
-        with open(filename, "w", encoding="UTF8", newline="") as f:
+        with open("Hand " + filename, "w", encoding="UTF8", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow(header)
+            writer.writerow(["a_x,a_y,a_z,f"])
             # write multiple rows
-            writer.writerows(self.data)
+            writer.writerows(self.data_on_bag)
 
-    def port_scan(self):
-        self.listPorts.clear()
+        with open("Bag " + filename, "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(["a_x,a_y,a_z,g_x,g_y,g_z"])
+            # write multiple rows
+            writer.writerows(self.data_on_hand)
+
+    def portScan(self):
         print("Start scaning")
         # r = executor.submit(self.serial_ports)
         r = self.serial_ports()
+        print(r)
         if len(r):
-            self.listPorts.addItems(r)
+            self.portOnHand.clear()
+            self.portOnBag.clear()
+            self.portOnHand.addItems(r)
+            self.portOnBag.addItems(r)
+            self.portOnHand.currentTextChanged.connect(self.portOnHandChange)
+            self.portOnBag.currentTextChanged.connect(self.portOnBagChange)
             print("Scanning completeted")
-            self.listPorts.itemClicked.connect(self.getItem)
         else:
             print("No ports available")
 
-    def getItem(self):
-        print("Selected port: " + self.listPorts.currentItem().text())
-        self.port = self.listPorts.currentItem().text()
+    def portOnHandChange(self, s):
+        self.port_on_bag = s
+
+    def portOnBagChange(self, s):
+        self.port_on_hand = s
 
     # https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
     def serial_ports(self):
@@ -102,8 +127,9 @@ class MainWindow(QtWidgets.QMainWindow):
         """
         if sys.platform.startswith("win"):
             # ports = ['COM%s' % (i + 1) for i in range(256)]
-            ports = ["COM%s" % (i + 1) for i in range(8)]
-        elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
+            ports = ["COM%s" % (i + 1) for i in range(10)]
+        elif sys.platform.startswith("linux") or sys.platform.startswith(
+                "cygwin"):
             # this excludes your current terminal "/dev/tty"
             ports = glob.glob("/dev/tty[A-Za-z]*")
         elif sys.platform.startswith("darwin"):
