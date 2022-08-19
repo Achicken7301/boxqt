f1a330e5c746f48aeb4d75d371137084262b5bad
Khang <55547021+Achicken7301@users.noreply.github.com>
2022-08-19 16:01:29 +0700

    Feat(main.py) port scan faster
    https://stackoverflow.com/questions/55816358/how-to-obtain-bluetooth-port-direction-with-pyserial

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from PyQt5 import QtWidgets, uic, QtCore
 import sys
-import glob
 import serial
 import datetime
 import csv
-import time
+import serial.tools.list_ports
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -96,7 +95,6 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def portScan(self):
         print("Start scaning")
-        # r = executor.submit(self.serial_ports)
         r = self.serial_ports()
         print(r)
         if len(r):
@@ -106,46 +104,35 @@ class MainWindow(QtWidgets.QMainWindow):
             self.portOnBag.addItems(r)
             self.portOnHand.currentTextChanged.connect(self.portOnHandChange)
             self.portOnBag.currentTextChanged.connect(self.portOnBagChange)
+            self.port_on_bag = self.portOnHand.currentText()
+            self.port_on_hand = self.portOnHand.currentText()
             print("Scanning completeted")
         else:
             print("No ports available")
 
-    def portOnHandChange(self, s):
-        self.port_on_bag = s
+    def portOnHandChange(self):
+        self.port_on_bag = self.portOnHand.currentText()
 
-    def portOnBagChange(self, s):
-        self.port_on_hand = s
+    def portOnBagChange(self):
+        self.port_on_hand = self.portOnHand.currentText()
 
-    # https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+    # https://stackoverflow.com/questions/55816358/how-to-obtain-bluetooth-port-direction-with-pyserial
     def serial_ports(self):
-        """Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-        """
-        if sys.platform.startswith("win"):
-            # ports = ['COM%s' % (i + 1) for i in range(256)]
-            ports = ["COM%s" % (i + 1) for i in range(10)]
-        elif sys.platform.startswith("linux") or sys.platform.startswith(
-                "cygwin"):
-            # this excludes your current terminal "/dev/tty"
-            ports = glob.glob("/dev/tty[A-Za-z]*")
-        elif sys.platform.startswith("darwin"):
-            ports = glob.glob("/dev/tty.*")
-        else:
-            raise EnvironmentError("Unsupported platform")
-
-        result = []
-        for port in ports:
-            try:
-                s = serial.Serial(port)
-                s.close()
-                result.append(port)
-            except (OSError, serial.SerialException):
-                pass
-        return result
+        port = []
+        cp = serial.tools.list_ports.comports()
+        for p in cp:
+            if "BTHENUM" in p.hwid:
+                start_of_address = p.hwid.rfind("&")
+                end_of_address = p.hwid.rfind("_")
+                address = p.hwid[start_of_address + 1:end_of_address]
+                if int(address, 16) == 0:
+                    port_type = "incoming"
+                else:
+                    port_type = "outgoing"
+                # get Outgoing ports ONLY
+                if port_type == "outgoing":
+                    port.append(p.name)
+        return port
 
 
 def main():
