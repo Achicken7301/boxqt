b9aa28dee5393bc3494f4f4e12a69a01fcbea6ff
Khang <55547021+Achicken7301@users.noreply.github.com>
2022-09-09 11:39:07 +0700

    Test

--- arduino/testing/force/force.ino
+++ arduino/testing/force/force.ino
@@ -2,6 +2,13 @@
 const int potPin = 2;
 #include "SparkFun_LIS331.h"
 #include <Wire.h>
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
 
 LIS331 xl;
 // variable for storing the potentiometer value
@@ -9,6 +16,7 @@ int potValue = 0;
 
 void setup() {
   pinMode(potPin, INPUT);
+  SerialBT.begin("DeviceOnHand"); //Bluetooth device name
   Wire.begin();
   xl.setI2CAddr(0x19);
   xl.begin(LIS331::USE_I2C);
@@ -18,7 +26,7 @@ void setup() {
 void loop() {
   // Reading potentiometer value
   int potValue = analogRead(potPin);
-  
+
   int16_t x, y, z;
   xl.readAxes(x, y, z);  // The readAxes() function transfers the
 
@@ -27,9 +35,13 @@ void loop() {
   //  float result = (float(maxScale) * float(reading))/2047;
   //  return result;
   //}
-  //  200 = maxScale
+  //  100 = maxScale
   Serial.print(xl.convertToG(100, x)); Serial.print(" ");
   Serial.print(xl.convertToG(100, y)); Serial.print(" ");
   Serial.print(xl.convertToG(100, z)); Serial.print(" ");
   Serial.println(potValue);
+  SerialBT.print(xl.convertToG(100, x)); SerialBT.print(" ");
+  SerialBT.print(xl.convertToG(100, y)); SerialBT.print(" ");
+  SerialBT.print(xl.convertToG(100, z)); SerialBT.print(" ");
+  SerialBT.println(potValue);
 }
