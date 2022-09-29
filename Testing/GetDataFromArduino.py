5bd2a21ce7c77bb2916e875a5c3f61690cd03518
Khang <55547021+Achicken7301@users.noreply.github.com>
2022-09-29 21:43:55 +0700

    check sample rate of a COM port

--- Testing/GetDataFromArduino.py
+++ Testing/GetDataFromArduino.py
@@ -1,6 +1,22 @@
 import serial
-print(serial.version)
-# ser = serial.Serial(port='COM4', baudrate=115200)
-# b = ser.readline()
-# print(b)
-# ser.close()
+import serial.tools.list_ports
+import time
+
+# print(serial.version)
+ser = serial.Serial(port='COM7', baudrate=115200)
+# ser = serial.Serial(port='COM13', baudrate=115200)
+
+b = ser.readline()
+
+now = time.time()
+future = now + 1
+data = []
+while now < future:
+    # do stuff
+    now = time.time()
+    b = ser.readline()
+    data_ser = b.decode().splitlines()
+    data.append(data_ser)
+    print(str(future - now))
+# print(data)
+print("Sample rate: " + str(len(data)))
