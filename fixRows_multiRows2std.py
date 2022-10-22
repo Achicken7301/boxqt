29fad11c90056e8fc8860a81d9e73807ee44680e
Khang <55547021+Achicken7301@users.noreply.github.com>
2022-10-22 20:56:53 +0700

    update

--- fixRows_multiRows2std.py
+++ fixRows_multiRows2std.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import csv
 
-df = pd.read_excel("data\\clean_data\\raw_data.xlsx", sheet_name="input")
-
-final = []
+df = pd.read_excel("data\clean_data\\raw_data.xlsx", sheet_name="input")
 '''
 Header removed
 [0:31] -> row_#2 -> row_#32 => 31 rows
@@ -12,11 +10,10 @@ Header removed
 # print(df[31:62])
 # print(df[62:93])
 
+final = []
 number_of_rows = 31
-rows = []
 
 for i in range(0, len(df), number_of_rows):
-    rows.append(i)
     std = df[i:i + number_of_rows].std().values
     final.append(std)
 
@@ -24,8 +21,7 @@ for i in range(0, len(df), number_of_rows):
 
 header = ['std_ax', 'std_ay', 'std_az', 'std_gx', 'std_gy', 'std_gz']
 
-with open('data\\clean_data\\std.csv', 'w') as f:
-
+with open('std.csv', 'w') as f:
     # using csv.writer method from CSV package
     write = csv.writer(f)
     write.writerow(header)
